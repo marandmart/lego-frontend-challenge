44fac8cef0c9870c1686ff5f62c2ef1719a07f85
Mario Andre Martins <mario.andre.m@gmail.com>
2024-05-26 21:31:12 -0300

    refactoring

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,15 +8,7 @@ import homeIcon from "../../assets/icons/home.svg";
 import Title from "../Title";
 import Text from "../Text";
 import NavBar from "../NavBar";
-import architecture from "./assets/logos/architecture_logo.png";
-import city from "./assets/logos/city_logo.png";
-import creator from "./assets/logos/creator_logo.png";
-import dots from "./assets/logos/dots_logo.png";
-import duplo from "./assets/logos/duplo_logo.png";
-import friends from "./assets/logos/friends_logo.png";
-import ideas from "./assets/logos/ideas_logo.png";
-import minifigures from "./assets/logos/minifigures_logo.png";
-import technic from "./assets/logos/technic_logo.png";
+import { logos } from "./utils";
 
 interface ModalProps {
   isOpen: boolean;
@@ -47,18 +39,6 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
     setIsVisible(false);
     setTimeout(onClose, 300);
   };
-
-  const logos = [
-    { src: architecture, alt: "architecture" },
-    { src: city, alt: "city" },
-    { src: creator, alt: "creator" },
-    { src: dots, alt: "dots" },
-    { src: duplo, alt: "duplo" },
-    { src: friends, alt: "friends" },
-    { src: ideas, alt: "ideas" },
-    { src: minifigures, alt: "minifigures" },
-    { src: technic, alt: "technic" },
-  ];
 
   return (
     <div className={`${styles.modal} ${isVisible ? styles.visible : ""}`}>
---
+++ src/components/Modal/utils.ts
@@ -0,0 +1,21 @@
+import architecture from "./assets/logos/architecture_logo.png";
+import city from "./assets/logos/city_logo.png";
+import creator from "./assets/logos/creator_logo.png";
+import dots from "./assets/logos/dots_logo.png";
+import duplo from "./assets/logos/duplo_logo.png";
+import friends from "./assets/logos/friends_logo.png";
+import ideas from "./assets/logos/ideas_logo.png";
+import minifigures from "./assets/logos/minifigures_logo.png";
+import technic from "./assets/logos/technic_logo.png";
+
+export const logos = [
+  { src: architecture, alt: "architecture" },
+  { src: city, alt: "city" },
+  { src: creator, alt: "creator" },
+  { src: dots, alt: "dots" },
+  { src: duplo, alt: "duplo" },
+  { src: friends, alt: "friends" },
+  { src: ideas, alt: "ideas" },
+  { src: minifigures, alt: "minifigures" },
+  { src: technic, alt: "technic" },
+];
